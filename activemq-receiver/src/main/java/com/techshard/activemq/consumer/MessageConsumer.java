dd905fb668fceeb120f2bb5fa5ebf7704d79d9c9
Swathi Prasad <swathi.prasad88@gmail.com>
2019-08-07 23:01:11 +0200

    Update MessageConsumer.java

--- activemq-receiver/src/main/java/com/techshard/activemq/consumer/MessageConsumer.java
+++ activemq-receiver/src/main/java/com/techshard/activemq/consumer/MessageConsumer.java
@@ -2,10 +2,12 @@ package com.techshard.activemq.consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@EnableJms
 public class MessageConsumer {
 
     private final Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
